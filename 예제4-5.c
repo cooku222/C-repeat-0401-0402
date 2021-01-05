c72dc8e5493689b638e9b3e3d5e1f0df64861db3
cooku222 <68459662+cooku222@users.noreply.github.com>
2021-01-05 18:56:21 +0900

    Add files via upload

---
+++ 예제4-5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10, b = 20, c = 10;
+	int res;				//°á±£°ªÀ» ÀúÀåÇÒ º¯¼ö
+
+	res = (a > b);
+	printf("a>b:%d\n", res);
+	res = (a >= b);		//10>=20Àº °ÅÁþÀÌ¹Ç·Î °á±£°ªÀº 0
+	printf("a>=b:%d\n", res);
+	res = (a < b);		//10<20Àº ÂüÀÌ¹Ç·Î °á±£°ªÀº 1
+	printf("a<b:%d\n", res);
+	res = (a <= b);		//10<=20Àº ÂüÀÌ¹Ç·Î °á±£°ªÀº 1
+	printf("a<=b:%d\n", res);
+	res = (a <= c);		//10<=10Àº ÂüÀÌ¹Ç·Î °á±£°ªÀº 1
+	printf("a<=c:%d\n", res);
+	res = (a == b);		//10==20Àº °ÅÁþÀÌ¹Ç·Î °á±£°ªÀº 0
+	printf("a==b:%d\n", res);	
+	res = (a != c);		//10!=10Àº °ÅÁþÀÌ¹Ç·Î °á±£°ªÀº 0
+	printf("a!=c:%d\n", res);
+
+	return 0;
+}
